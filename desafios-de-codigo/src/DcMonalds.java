34e2cb0b2cf704fb0ee9dd40552f09c5ecb3ba2c
barbarahellen <barbarahellen993@gmail.com>
2023-02-22 11:57:54 -0300

    update dcMonalds

--- desafios-de-codigo/src/DcMonalds.java
+++ desafios-de-codigo/src/DcMonalds.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 
 /*
-    Você levou sua namorada na incrível rede de lanchonetes Dc Monalds e ficou maravilhado como aquele lanche era gostoso. Porém, ficou horrorizado como algumas coisas no aplicativo do cardápio estavam erradas. Então, você se ofereceu para ajudá-los com seus conhecimentos de programação. Ao conversar com o gerente, ele te explicou que a vontade da lanchonete era criar um menu com os nomes dos ingredientes formando o lanche, como no exemplo abaixo:
+    Você levou sua namorada na incrível rede de lanchonetes Dc Monalds e ficou maravilhado como aquele lanche era
+    gostoso. Porém, ficou horrorizado como algumas coisas no aplicativo do cardápio estavam erradas. Então, você se
+    ofereceu para ajudá-los com seus conhecimentos de programação. Ao conversar com o gerente, ele te explicou que
+    a vontade da lanchonete era criar um menu com os nomes dos ingredientes formando o lanche, como no exemplo abaixo:
 
     Pao
     Presunto
